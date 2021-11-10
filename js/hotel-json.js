cebf63fe6b9fea4d850f5200df3e377545c86796
josephjohnson42 <jmj18@usca.edu>
2021-11-10 12:59:35 -0500

    11-10 commit

--- js/hotel-json.js
+++ js/hotel-json.js
@@ -1,33 +1,37 @@
 var firebaseConfig = {
-  apiKey: "AIzaSyAzcwgZuLA7dO9g4sQhXQVTUgCo0M8m2qM",
-  authDomain: "grocerylist-91956.firebaseapp.com",
-  databaseURL: "https://grocerylist-91956.firebaseio.com",
-  projectId: "grocerylist-91956",
-  storageBucket: "grocerylist-91956.appspot.com",
-  messagingSenderId: "813812426276",
-  appId: "1:813812426276:web:93e5897af12892ff78dab1",
-  measurementId: "G-VZ83BTR72T"
+  apiKey: 'AIzaSyBKpDfpZ-E0zVh9-Iw5GgO94wqiktFKa08',
+  authDomain: 'csci225-josephjohnson.firebaseapp.com',
+  projectId: 'csci225-josephjohnson',
+  storageBucket: 'csci225-josephjohnson.appspot.com',
+  messagingSenderId: '449711620730',
+  appId: '1:449711620730:web:57d0b17e63f389722f71c3',
+  measurementId: 'G-PPGN5EDK91',
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-/* object examples 
+/* object examples */
 var testJson = {};
-testJson["lastname"] = "zhang";
-testJson["location"] = "aiken";
+testJson['lastname'] = 'zhang';
+testJson['location'] = 'aiken';
 console.log(testJson);
-*/
 
 // enter data in
-$("input[type='button']").click(function(e) {
+$("input[type='button']").click(function (e) {
   //get the value of form
-  
-  
+  const inputdata = $('form').serializeArray();
+  console.log(inputdata);
+
   /* save the data to database */
-
-
+  var inputJson = {};
+  inputJson['name'] = 'joseph';
+  inputJson['checkin'] = '2021-11-15';
+  inputJson['checkout'] = '2021-11-20';
+  inputJson['guests'] = '5';
+  inputJson['rType'] = 'Grand Suite $289';
+  firebase.firestore().collection('hotelreservation').add(inputJson); //saves the data
 
   /* clear the entry */
-  $("form")[0].reset();
+  $('form')[0].reset();
 });
 
 /* array example
@@ -39,10 +43,10 @@ array1.forEach(element => console.log(element));
 
 firebase
   .firestore()
-  .collection("hoteldata")
-  .onSnapshot(querySnapshot => {
+  .collection('hoteldata')
+  .onSnapshot((querySnapshot) => {
     console.log(querySnapshot.size);
-    querySnapshot.forEach(doc => {
+    querySnapshot.forEach((doc) => {
       console.log(doc.data());
       console.log(doc.data().room);
       console.log(doc.data().checkout);
